895ab0b221dc977cc919d015a7f94a206602ed51
Alex Bigelow <alex.r.bigelow@gmail.com>
2019-09-24 09:43:45 -0700

    Fix missing check

--- src/View.js
+++ src/View.js
@@ -23,7 +23,8 @@ class View extends Model {
       });
     }
     await this.ready;
-    if (this.dirty || d3el.node() !== this.d3el.node()) {
+    if ((this.dirty || d3el.node() !== this.d3el.node()) &&
+        this._setupPromise === undefined) {
       // Need a fresh render; call setup immediately
       this.updateContainerCharacteristics(d3el);
       this._setupPromise = this.setup(d3el);
